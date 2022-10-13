b45427c8d9bc5d3d5fc002bd6379d9b5d6c19024
sreadytocode <sara.younis2303@gmail.com>
2022-10-13 13:04:16 +0100

    Completed last testing file for garment class

--- run_tests.py
+++ run_tests.py
@@ -3,6 +3,7 @@ import unittest
 from tests.brand_test import TestBrand
 from tests.image_test import TestImage
 from tests.type_test import TestType
+from tests.garment_test import TestGarment
 
 
 if __name__ == '__main__':
--- tests/garment_test.py
+++ tests/garment_test.py
@@ -1,3 +1,32 @@
 import unittest
 from models.garment import Garment
 
+class TestGarment(unittest.TestCase):
+
+    def setUp(self):
+        self.garment = Garment("Mint and gold dress", 2, 3, "Celestial mint and gold dress with embroidered chiffon and silk.", 15, 100.00, 165.00)
+
+    def test_garment_has_name(self):
+        self.assertEqual("Mint and gold dress", self.garment.name)
+
+    def test_garment_has_allocated_brand(self):
+        self.assertEqual(2, self.garment.brand)
+
+    def test_garment_has_allocated_type(self):
+        self.assertEqual(3, self.garment.type)
+
+    def test_garment_has_description(self):
+        self.assertEqual("Celestial mint and gold dress with embroidered chiffon and silk.", self.garment.description)
+
+    def test_garment_has_stock_quantity(self):
+        self.assertEqual(15, self.garment.stock_quantity)
+
+    def test_garment_has_buying_cost(self):
+        self.assertEqual(100.00, self.garment.buying_cost)
+
+    def test_garment_has_selling_price(self):
+        self.assertEqual(165.00, self.garment.selling_price)
+
+    def test_garment_can_calculate_markup(self):
+        result = self.garment.calculate_markup()
+        self.assertEqual(65.00, result)
